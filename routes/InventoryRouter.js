5b816899a9687b4f74e3166987c28ccf62fdf2a0
Amotul-raheem <amotulraheem1499@gmail.com>
2022-05-19 17:07:10 +0100

    created edit inventory end point

--- routes/InventoryRouter.js
+++ routes/InventoryRouter.js
@@ -16,33 +16,56 @@ inventoryRouter.post("/create-inventory", async (req, res) => {
             price_per_unit: req.body.price_per_unit
         })
         await product.save();
-        console.log("product inventory saved")
         res.status(200).send("Product inventory saved successfully.")
     } catch (error) {
-        console.log("error saving inventory")
         res.status(500).send(error)
     }
 })
 
 inventoryRouter.post("/edit-inventory", async (req, res) => {
-    const product = await Product.findOne({product_code: req.body.product_code});
+    try {
 
+        const product = await Product.findOne({product_code: req.body.product_code});
+        let inventoryOnHand = product.inventory_on_hand
+        const type_of_edit = req.body.edit_type
 
+        if (type_of_edit === "add") {
+            inventoryOnHand += parseInt(req.body.quantity_of_product)
+        } else if (type_of_edit === "subtract"){
+            inventoryOnHand -= req.body.quantity_of_product
+        }else {
+            res.status(401).send("invalid argument")
+        }
+
+        if (inventoryOnHand < 0) {
+            res.status(400).send("unable to edit inventory");
+        }
+
+        product.inventory_on_hand = inventoryOnHand
+        product.save()
+
+        res.status(200).send("Inventory edited successfully")
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 inventoryRouter.get("/get-all-inventory", async (req, res) => {
     try {
         const productsInventory = await Product.find({deleted: {$ne: true}})
 
         res.status(200).send(productsInventory)
-        console.log("list of inventory sent")
-
     } catch (error) {
         res.status(500).send(error)
     }
 })
 inventoryRouter.post("/delete-inventory", async (req, res) => {
     try {
-        const product = await product.findOne({product_code: req.body.product_code});
+        const productCode = req.body.product_code
+        if (!productCode) {
+            res.status(400).send("Invalid product");
+        }
+
+        const product = await Product.findOne({product_code: productCode});
 
         product.deleted = true
         product.deletion_message = req.body.deletion_message
@@ -53,16 +76,20 @@ inventoryRouter.post("/delete-inventory", async (req, res) => {
         res.status(500).send(error)
     }
 })
-inventoryRouter.post("/undelete-inventory", async (req,res) => {
-    try{
-        const product = await product.findOne({product_code: req.body.product_code});
+inventoryRouter.post("/undelete-inventory", async (req, res) => {
+    try {
+        const productCode = req.body.product_code
+        if (!productCode) {
+            res.status(400).send("Invalid product");
+        }
+        const product = await Product.findOne({product_code: req.body.product_code});
 
         product.deleted = false
         product.deletion_message = null
         await product.save()
 
         res.status(200).send("Inventory undeleted successfully")
-    } catch (error){
+    } catch (error) {
         res.status(500).send(error)
     }
 })
